43e3a3f9b52b8bd7619bf6ee26a766ffbb81064f
Roman Petrashkevich <roman.petrashkevich@gmail.com>
2017-11-22 12:34:53 +0300

    Finished implementing GenieSingleBarActivity demo

--- MPChartExample/src/com/xxmassdeveloper/mpchartexample/GenieSingleBarActivity.java
+++ MPChartExample/src/com/xxmassdeveloper/mpchartexample/GenieSingleBarActivity.java
@@ -1,8 +1,11 @@
 package com.xxmassdeveloper.mpchartexample;
 
 import android.graphics.Shader;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
+import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.AxisBase;
@@ -16,6 +19,7 @@ import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.github.mikephil.charting.utils.GradientColor;
 import com.xxmassdeveloper.mpchartexample.notimportant.DemoBase;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,76 +30,102 @@ import java.util.List;
 
 public class GenieSingleBarActivity extends DemoBase {
 
-    protected BarChart mChart;
+    private int PARAM_GOAL = 5000;
+    private int PARAM_STEPS = 7500;
+
+    private static float CONST_UPPER_BOUND_MULT = 2.3f;
+    private static float CONST_LOWER_BOUND_MULT = -0.1f;
+    private static int CONST_GRANULARITY = 1000;
+    private static int CONST_GRANULARITY_LABELS = 10000;
+    private static int CONST_ANIMATION_TIME = 1500;
+    private static int CONST_FONT_SIZE = 15;
+    private static int CONST_LINE_WIDTH = 2;
+    private static int CONST_GRID_LENGTH = 32;
+    private static float CONST_GRID_LABEL_OFFSET_MULT = 0.5f;
+
+    private BarChart chart;
+    private TextView stepsLabel;
+
+    private DecimalFormat numberFormat = new DecimalFormat("#,###");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_genie_single);
 
-        mChart = findViewById(R.id.chart);
-        mChart.setDrawBarShadow(false);
-        mChart.setDrawValueAboveBar(false);
-        mChart.setTouchEnabled(false);
-        mChart.getDescription().setEnabled(false);
-        mChart.getLegend().setEnabled(false);
-        mChart.setDrawGridBackground(false);
-        mChart.getXAxis().setEnabled(false);
-        mChart.getAxisRight().setEnabled(false);
-        mChart.getAxisLeft().setDrawLabels(true);
-        mChart.setDrawAxisOnTopOfData(true);
-        mChart.setClipToViewPortBottomEnabled(true);
-        mChart.setViewPortOffsets(0f, 0f, 0f, 0f);
-        mChart.animateY(1000);
+        stepsLabel = findViewById(R.id.tv_steps_count);
+        chart = findViewById(R.id.chart);
 
-        YAxis leftAxis = mChart.getAxisLeft();
+        initViews();
+    }
+
+    private void initViews() {
+        Typeface boldFont = ResourcesCompat.getFont(this, R.font.gilroy_bold);
+
+        chart.setDrawBarShadow(false);
+        chart.setDrawValueAboveBar(false);
+        chart.setTouchEnabled(false);
+        chart.getDescription().setEnabled(false);
+        chart.getLegend().setEnabled(false);
+        chart.setDrawGridBackground(false);
+        chart.getXAxis().setEnabled(false);
+        chart.getAxisRight().setEnabled(false);
+        chart.getAxisLeft().setDrawLabels(true);
+        chart.setDrawAxisOnTopOfData(true);
+        chart.setClipToViewPortBottomEnabled(true);
+        chart.setViewPortOffsets(0f, 0f, 0f, 0f);
+        chart.animateY(CONST_ANIMATION_TIME);
+
+        YAxis leftAxis = chart.getAxisLeft();
         leftAxis.setLabelCount(10, false);
         leftAxis.setPosition(YAxis.YAxisLabelPosition.INSIDE_CHART);
-        leftAxis.setAxisMaximum(11500f);
-        leftAxis.setAxisMinimum(-500f);
-        leftAxis.setGranularity(1000f);
-        leftAxis.setTextSize(18);
+        leftAxis.setAxisMaximum(PARAM_GOAL * CONST_UPPER_BOUND_MULT);
+        leftAxis.setAxisMinimum(PARAM_GOAL * CONST_LOWER_BOUND_MULT);
+        leftAxis.setGranularity(CONST_GRANULARITY);
+        leftAxis.setTypeface(boldFont);
+        leftAxis.setTextSize(CONST_FONT_SIZE);
         leftAxis.setTextColor(ContextCompat.getColor(this, android.R.color.white));
-        leftAxis.setXOffset(25);
-        leftAxis.setGridLineWidth(3);
+        leftAxis.setXOffset(CONST_GRID_LABEL_OFFSET_MULT * CONST_GRID_LENGTH);
+        leftAxis.setGridLineWidth(CONST_LINE_WIDTH);
         leftAxis.setGridColor(ContextCompat.getColor(this, android.R.color.white));
-        leftAxis.enableGridDashedLine(50, 1000000, 0);
+        leftAxis.enableGridDashedLine(CONST_GRID_LENGTH, 1000000, 0);
         leftAxis.setValueFormatter(new IAxisValueFormatter() {
             @Override
             public String getFormattedValue(float value, AxisBase axis) {
-                return value%10000 == 0 ? String.valueOf((int)value) : "";
+                return value% CONST_GRANULARITY_LABELS == 0 ? numberFormat.format(value) : "";
             }
         });
 
-        LimitLine limitLine = new LimitLine(5000, "Goal: 5000");
+        LimitLine limitLine = new LimitLine(PARAM_GOAL,
+                getString(R.string.genie_poc_label_goal) + ": " + numberFormat.format(PARAM_GOAL));
         limitLine.setLabelPosition(LimitLine.LimitLabelPosition.IMAGE_CENTER);
-        limitLine.setLineWidth(3);
+        limitLine.setLineWidth(CONST_LINE_WIDTH);
         limitLine.setLineColor(ContextCompat.getColor(this, R.color.genie_chart_limit_line));
         limitLine.setTextColor(ContextCompat.getColor(this, android.R.color.white));
-        limitLine.setTextSize(15);
+        limitLine.setTypeface(boldFont);
+        limitLine.setTextSize(CONST_FONT_SIZE);
         limitLine.setImageRes(R.drawable.np_trophy_888734_000000);
         leftAxis.addLimitLine(limitLine);
 
-        List<Integer> steps = new ArrayList<>();
-        steps.add(7500);
-        setData(steps);
+        setData(PARAM_STEPS);
     }
 
-    private void setData(List<Integer> steps) {
+    private void setData(int steps) {
+        String formatted = numberFormat.format(steps);
+        stepsLabel.setText(formatted);
+
         ArrayList<BarEntry> yValues = new ArrayList<>();
-        for (int i = 0; i < steps.size(); i++) {
-            yValues.add(new BarEntry(i, steps.get(i)));
-        }
+        yValues.add(new BarEntry(0, steps));
 
         BarDataSet dataSet;
 
-        if (mChart.getData() != null && mChart.getData().getDataSetCount() > 0) {
-            dataSet = (BarDataSet) mChart.getData().getDataSetByIndex(0);
+        if (chart.getData() != null && chart.getData().getDataSetCount() > 0) {
+            dataSet = (BarDataSet) chart.getData().getDataSetByIndex(0);
             dataSet.setValues(yValues);
-            mChart.getData().notifyDataChanged();
-            mChart.notifyDataSetChanged();
+            chart.getData().notifyDataChanged();
+            chart.notifyDataSetChanged();
         } else {
-            dataSet = new BarDataSet(yValues, "Steps");
+            dataSet = new BarDataSet(yValues, getString(R.string.genie_poc_label_steps));
             dataSet.setDrawIcons(false);
 
             List<GradientColor> gradients = new ArrayList<>();
@@ -115,7 +145,7 @@ public class GenieSingleBarActivity extends DemoBase {
             data.setDrawValues(false);
             data.setBarWidth(1.0f);
 
-            mChart.setData(data);
+            chart.setData(data);
         }
     }
 }
